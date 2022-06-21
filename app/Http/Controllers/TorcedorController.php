6da82fcebf0b3820bada71ade0d48bc42c7ecdba
Gabriel Viero <gabrielviero22@gmail.com>
2022-06-20 21:10:18 -0300

    Torcedor Mod

--- app/Http/Controllers/TorcedorController.php
+++ app/Http/Controllers/TorcedorController.php
@@ -8,27 +8,59 @@ use App\Http\Requests\UpdateTorcedorRequest;
 use App\Models\Torcedor;
 
 class TorcedorController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
     {
-        //
+        /**
+         * Display a listing of the resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function index()
+        { 
+            $lista = Torcedor::all();
+            //dd($lista);
+            return view('torcedor.index',['lista_torcedor'=>$lista]);
+        }
+    
+    //     /**
+    //      * Show the form for creating a new resource.
+    //      *
+    //      * @return \Illuminate\Http\Response
+    //      */
+        public function create()
+        {
+            return view('torcedor.create');
+        }
+        public function store(Request $request)
+        {
+            // dd($request);
+            Torcedor::create($request->all());
+            return redirect()->route('Torcedor.index');
+        }
+        public function destroy($id)
+        {
+            // dd($id);
+            $torcedor = Torcedor::findOrFail($id);
+            $torcedor->delete();
+            return  redirect()->route('torcedor.index');
+        }
+        public function edit($id)
+        {
+            // dd($id);
+            $torcedor = Torcedor::findOrFail($id);
+            return view('Torcedor.edit',['Torcedor'=>$torcedor]);
+        }
+        public function update(Request $request, $id)
+        {
+            // dd($id);
+            $torcedor = Torcedor::findOrFail($id);
+            $data = [
+                'nome'=> $request['nome'],
+            ];
+            $torcedor->update($data);
+            return redirect()->route('torcedor.index');
+        }
     }
-}
 //     /**
 //      * Store a newly created resource in storage.
 //      *
--- app/Models/Torcedor.php
+++ app/Models/Torcedor.php
@@ -10,7 +10,7 @@ class Torcedor extends Model
     use HasFactory;
 
     protected $fillable = [
-        'nome',
-        'fundacao'
+        'nome'
+        
     ];
 }
